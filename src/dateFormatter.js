018df67f03ad5184cb4a9c297442c9e2c268f695
Martin Gasiorowski <mjg1185@gmail.com>
2019-06-03 10:09:11 +0100

    Update dateFormatter.js
    Branch for modifying formatDate function.

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,11 @@
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+  let formatDate = new Date(dateToFormatTimeMillis);
+  let sysDate= new Date(systemDateTimeMillis);
+  if((formatDate - sysDate) == 0){
+    //dates are the same
+    return 'TODAY'; 
+  }else{
+    //dates are not the same, return formatted dateToFormatTimeMillis to DD/MM/YYYY
+    return formatDate.getDate() + '/' + formatDate.getMonth() + '/' + formatDate.getFullYear();
+  }
 };
